bc5f45ba48b8d3a7ba1a513f65f6194cb86f5b58
unknown <ianfelipinhonet@gmail.com>
2023-07-10 00:00:42 -0300

    alterando o layout do desktop-1

---
+++ script/card_description.js
@@ -0,0 +1,17 @@
+function mostra() {
+    let curiosidades = document.querySelector('#promo1 p');
+    if (curiosidades.style.display === 'none') {
+      curiosidades.style.display = 'block';
+    } else {
+      curiosidades.style.display = 'none';
+    }
+}
+
+function mostra1() {
+    let curiosidades = document.querySelector('#promo2 p');
+    if (curiosidades.style.display === 'none') {
+      curiosidades.style.display = 'block';
+    } else {
+      curiosidades.style.display = 'none';
+    }
+}
